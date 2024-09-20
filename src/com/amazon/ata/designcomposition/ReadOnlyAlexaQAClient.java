3a4f69c0530cc5e052035912c3a6115973642562
QuantumCodings <166426987+QuantumCodings@users.noreply.github.com>
2024-09-20 18:21:44 -0400

    Update ReadOnlyAlexaQAClient.java
    nice

--- src/com/amazon/ata/designcomposition/ReadOnlyAlexaQAClient.java
+++ src/com/amazon/ata/designcomposition/ReadOnlyAlexaQAClient.java
@@ -1,8 +1,19 @@
 package com.amazon.ata.designcomposition;
 
-public class ReadOnlyAlexaQAClient extends AlexaQAClient {
-    @Override
-    public void submitQuestionAnswerSuggestion(AlexaQuestion question, AlexaAnswer answer) {
-        // Do nothing
+public class ReadOnlyAlexaQAClient {
+    private final AlexaQAClient alexaQAClient;
+
+    public ReadOnlyAlexaQAClient(AlexaQAClient alexaQAClient) {
+        this.alexaQAClient = alexaQAClient;
     }
+
+    // Example read methods (assuming these exist in AlexaQAClient)
+    public AlexaAnswer getAnswerForQuestion(AlexaQuestion question) {
+        return alexaQAClient.getAnswerForQuestion(question);
+    }
+
+    public List<AlexaQuestion> getAllQuestions() {
+        return alexaQAClient.getAllQuestions();
+    }
+
 }
